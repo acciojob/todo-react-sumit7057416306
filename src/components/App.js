e6dcaae1d3a716a642425de45f8f0a6433a14606
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:13:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
 
-const App = () => {
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+
+  const handleRemoveTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>To-Do List:</p>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <div className="item">
+            <li key={index}>
+              {todo}
+            </li>
+            <button onClick={() => handleRemoveTodo(index)}>Delete</button>
+          </div>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
